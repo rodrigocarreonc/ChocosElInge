83bf6cafc1f625d9ef92e1ddbc48ea9dfde946d9
rodrigocarreonc <rodrigocarreon21@gmail.com>
2025-01-09 15:19:11 -0600

    Add jwt auth and protected endpoints with that

---
+++ api/app/Models/Admin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+    public $timestamps = false;
+
+    protected $table = 'admin';
+
+    protected $fillable = ['name', 'username', 'password'];
+
+    protected $hidden = ['password'];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\SaborController;
 use App\Http\Controllers\VentaController;
 use App\Http\Controllers\BalanceController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,23 +23,56 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
 
-Route::get('productos', [ProductoController::class,'all']);
-Route::post('productos', [ProductoController::class,'create']);
-Route::put('productos/{id}', [ProductoController::class,'update']);
-Route::delete('productos/{id}', [ProductoController::class,'delete']);
+    'middleware' => 'api',
+    'prefix' => 'auth'
 
-Route::get('sabores', [SaborController::class,'get']);
-Route::post('sabores', [SaborController::class,'create']);
-Route::put('sabores/{id}', [SaborController::class,'update']);
-Route::delete('sabores/{id}', [SaborController::class,'delete']);
+], function ($router) {
 
-Route::get('ventas', [VentaController::class,'all']);
-Route::get('ventas/{id}', [VentaController::class,'get']);
-Route::post('venta', [VentaController::class,'create']);
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('register', [AuthController::class,'register']);
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::get('me', [AuthController::class,'me']);
 
-Route::get('egreso', [BalanceController::class,'get']);
-Route::post('egreso', [BalanceController::class,'outflow']);
-Route::delete('egreso/{id}', [BalanceController::class,'delete']);
+});
 
-Route::get('balance', [BalanceController::class,'balance']);
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'product'
+], function ($router) {
+    Route::get('productos', [ProductoController::class,'all']);
+    Route::post('productos', [ProductoController::class,'create']);
+    Route::put('productos/{id}', [ProductoController::class,'update']);
+    Route::delete('productos/{id}', [ProductoController::class,'delete']);
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'taste'
+], function ($router) {
+    Route::get('sabores', [SaborController::class,'get']);
+    Route::post('sabores', [SaborController::class,'create']);
+    Route::put('sabores/{id}', [SaborController::class,'update']);
+    Route::delete('sabores/{id}', [SaborController::class,'delete']);
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'sale'
+], function ($router) {
+    Route::get('ventas', [VentaController::class,'all']);
+    Route::get('ventas/{id}', [VentaController::class,'get']);
+    Route::post('venta', [VentaController::class,'create']);
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'finance'
+], function ($router) {
+    Route::get('egreso', [BalanceController::class,'get']);
+    Route::post('egreso', [BalanceController::class,'outflow']);
+    Route::delete('egreso/{id}', [BalanceController::class,'delete']);
+    Route::get('balance', [BalanceController::class,'balance']);
+});
